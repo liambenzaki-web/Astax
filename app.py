7fbbda2b73abf656715d5e08fa66b6f8238e095e
liambenzaki-web <liambenzaki@gmail.com>
2026-03-28 22:17:12 +0100

    Update app.py

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ import io
 import time
 
 API_TOKEN = os.environ.get("HF_TOKEN")
-API_URL = "https://api-inference.huggingface.co/models/stabilityai/stable-diffusion-xl-base-1.0"
+API_URL = "https://api-inference.huggingface.co/models/stabilityai/sdxl-turbo"
 
 headers = {"Authorization": f"Bearer {API_TOKEN}"}
 
